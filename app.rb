b839bff2412b313b4deffa875dc7608e94cd1f7b
andrewfritz86 <andrew.fritz86@gmail.com>
2014-09-09 09:41:56 -0400

    reconfig redis

--- app.rb
+++ app.rb
@@ -17,6 +17,7 @@ class App < Sinatra::Base
     enable :method_override
     enable :sessions
     REDIS_URL = "redis://127.0.0.1:6379"
+    uri = URI.parse("redis://127.0.0.1:6379")
     $redis = Redis.new({:host => uri.host,
                         :port => uri.port,
                         :password => uri.password})
